001efe98050c5720017a49e21802d3591cb672ea
Neil Gentleman <ngentleman@gmail.com>
2013-07-24 23:55:16 -0700

    OXO ends when a player has won

--- src/main/kotlin/com/github/nigelzor/mcts/OXOState.kt
+++ src/main/kotlin/com/github/nigelzor/mcts/OXOState.kt
@@ -18,6 +18,11 @@ public class OXOState: GameState<Int> {
 	}
 
 	override fun possible(): Set<Int> {
+		for ((x,y,z) in array(Triple(0, 1, 2), Triple(3, 4, 5), Triple(6, 7, 8), Triple(0, 3, 6), Triple(1, 4, 7), Triple(2, 5, 8), Triple(0, 4, 8), Triple(2, 4, 6))) {
+			if (board[x] != 0 && board[x] == board[y] && board[y] == board[z]) {
+				return setOf();
+			}
+		}
 		return board.indices.iterator().filter { (idx : Int): Boolean -> board[idx] == 0 }.toSet()
 	}
 
